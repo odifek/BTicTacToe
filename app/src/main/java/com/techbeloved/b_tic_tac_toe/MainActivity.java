0217378d38057f4af3710b77ff4839d5b278ed05
Odife K <kennedy998@gmail.com>
2018-04-01 23:52:22 +0100

    Made responsive by adding and configuring xml layouts for landscape orientation

--- app/src/main/java/com/techbeloved/b_tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/techbeloved/b_tic_tac_toe/MainActivity.java
@@ -175,7 +175,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Initialize board
         initializeGame();
         initializeGameBoard();
